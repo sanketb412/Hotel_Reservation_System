e48fc51323788c89c4f55d47e01f8e49606c4794
sanketb412 <sanketbagde38@gmail.com>
2021-05-21 08:31:59 +0530

    Added: code of Welcoming Message

---
+++ main/java/com/booking/reservationMessage.java
@@ -0,0 +1,7 @@
+package com.booking;
+
+public class reservationMessage {
+    public static void main(String[] args) {
+        System.out.println("..###..Welcome to Hotel Reservation..###...");
+    }
+}
---
+++ test/java/com/booking/hotelReservationSystem.java
@@ -0,0 +1,5 @@
+package com.booking;
+
+public class hotelReservationSystem {
+
+}
